8238bf74e3b4793e9329a9cb698fddcfc611252a
Juan Jose A. Zuluaga <juaninfante29@gmail.com>
2024-04-02 17:17:25 -0500

    init app react native

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Soccer   from './Cultivation/Cultivation';
+import { View , ImageBackground} from 'react-native';
+
+export default function App() {
+  return (
+    <ImageBackground 
+    source={{uri: 'https://th.bing.com/th/id/OIG2.In9jDPLaCbgTf56Rkv.W?pid=ImgGn'}}
+    style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+    resizeMode="cover"
+    >
+      <View style={{marginTop: 30}}>
+        <Soccer />
+      </View>
+    </ImageBackground>
+  );
+}
